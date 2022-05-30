b507babf764dca5514255543c31066a41f6d4e25
ianMizi <ianMizi09@gmail.com>
2022-05-30 13:37:24 +0800

    Add project files.

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
+
+namespace EmployeeService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            EnableCorsAttribute Cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(Cors);
+            config.Filters.Add(new BasicAuthenticationAttribute());
+           
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        //public IEnumerable<Employee> Get()
+        //{
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        return entities.Employees.ToList();
+        //    }
+        //}
+
+        //[BasicAuthentication]
+        //public HttpResponseMessage Get(string gender = "All")
+        //{
+        //    string username = Thread.CurrentPrincipal.Identity.Name;
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        switch (username.ToLower())
+        //        {
+        //            case "male":
+        //                return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower() == "male").ToList());
+        //            case "female":
+        //                return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
+        //            default:
+        //                return Request.CreateResponse(HttpStatusCode.BadRequest);
+        //        }
+        //    }
+        //}
+
+
+        public HttpResponseMessage Get()
+        {
+            string username = Thread.CurrentPrincipal.Identity.Name;
+            return Request.CreateResponse(HttpStatusCode.OK, username);
+            //using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            //{
+            //    //switch (username.ToLower())
+            //    //{
+            //    //    case "male":
+            //    //        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower() == "male").ToList());
+            //    //    case "female":
+            //    //        return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
+            //    //    default:
+            //    //        return Request.CreateResponse(HttpStatusCode.BadRequest);
+            //    //}
+
+            //    switch (username.ToLower())
+            //    {
+            //        case "male":
+            //            return Request.CreateResponse(HttpStatusCode.OK, username);
+            //        case "female":
+            //            return Request.CreateResponse(HttpStatusCode.OK, username);
+            //        default:
+            //            return Request.CreateResponse(HttpStatusCode.BadRequest);
+            //    }
+            //}
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee With ID:" + id + " Not Found!");
+                }
+            }
+        }
+
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                try
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+               
+            }
+
+        }
+
+
+    }
+}
